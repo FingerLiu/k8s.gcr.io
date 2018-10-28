04b8c7bf34e67ea961d4f9f5a5b9a81aa07c7e31
liupeng <liupeng@luojilab.com>
2018-10-28 20:49:08 +0800

    [ADD] add images

---
+++ pull.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "MAINTAINER liupeng <liupeng.dalian.gmail.com>" >> kube-apiserver/v1.12.1/Dockerfile
+echo "MAINTAINER liupeng <liupeng.dalian.gmail.com>" >> kube-controller-manager/v1.12.1/Dockerfile
+echo "MAINTAINER liupeng <liupeng.dalian.gmail.com>" >> kube-scheduler/v1.12.1/Dockerfile
+echo "MAINTAINER liupeng <liupeng.dalian.gmail.com>" >> kube-proxy/v1.12.1/Dockerfile
+echo "MAINTAINER liupeng <liupeng.dalian.gmail.com>" >> pause/3.1/Dockerfile
+echo "MAINTAINER liupeng <liupeng.dalian.gmail.com>" >> etcd/3.2.24/Dockerfile
+echo "MAINTAINER liupeng <liupeng.dalian.gmail.com>" >> coredns/1.2.2/Dockerfile
